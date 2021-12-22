5512011895d284c394f3bca2c43fa6ece21e0fd8
Chico Deme <chico.demetroff@forgerock.com>
2021-12-22 10:43:27 -0800

    updated description

--- amjourneytemplate.js
+++ amjourneytemplate.js
@@ -2,13 +2,15 @@
  *
  * Authors: se@forgerock.com
  * 
- * Description of the script goes here
+ * This script shows a default template on how your Scripting Decision Node scripts should be written.
+ * It shows you how to work with sharedState, transientState, secureState.
+ * This is a great debug script to use in and of itself as it will display all three states mentioned above in a formatted table
  * 
  * This script needs to be parametrized. It will not work properly as is. 
  * It requires some nodes that set at least sharedState before it can operate.
  * For example, set a page node with Platform Username and Platform Password nodes
  * 
- * The Scripted Decision Node needs the following outcomes defined:
+ * This Scripted Decision Node needs the following outcomes defined:
  * - true
  */
 
